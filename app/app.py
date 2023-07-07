17bc38a5367fcc35bb30a175df08be5dd0a295c7
Thiago Silva <thiago.silva@iesb.edu.br>
2023-07-07 15:55:10 -0300

    First Commit

---
+++ app/app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import pickle
+import os
+
+# Carregue o modelo a partir do arquivo pickle
+model_path = "C:/Users/thiago.silva/API/Projeto/Sistemas_Inteligentes/models/toxic_to_pet.pkl"
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
+
+
+def predict(imagem):
+    # Salva a imagem em um arquivo temporário
+    temp_file = "temp.png"
+    imagem.save(temp_file)
+    
+    img = image.load_img(temp_file, target_size=(96, 96))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+    class_index = np.argmax(classes)
+    class_names = ['cebola', 'alho', 'uva', 'passas', 'macadamia', 'massa_pao', 'lirio', 'azaleia', 'aloe_vera', 'filodendros', 'dieffenbachia', 'begonia', 'ciclamen', 'hortensia', 'comigo_ninguem_pode', 'coroa_de_cristo', 'abacate']
+    inferred_class = class_names[class_index]
+    
+    # Remove o arquivo temporário
+    os.remove(temp_file)
+    
+    return inferred_class, class_index
+
+
+def main():
+    st.title("Classificador de imagens de plantas e alimentos tóxicos para pets")
+    
+    # Exemplo de upload de imagem
+    uploaded_file = st.file_uploader("Selecione uma imagem", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Abre a imagem
+        imagem = Image.open(uploaded_file)
+        
+        # Exibe a imagem
+        st.image(imagem, caption="Imagem carregada")
+        
+        # Faz a previsão
+        predicted_class = predict(imagem)
+        
+        # Exibe a classe inferida
+        st.write("Classe inferida:", predicted_class[0])
+        st.write("Score:", predicted_class[1])
+
+if __name__ == "__main__":
+    main()
---
+++ app/functions.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Bibliotecas para o modelo
+import tensorflow as tf
+import keras_preprocessing
+from keras_preprocessing import image
+from keras_preprocessing.image import ImageDataGenerator
+import pickle
+import numpy as np
+
+def treino():
+    diretorio_raiz = 'C:/Users/thiago.silva/API/Projeto/Sistemas_Inteligentes/data/dados_tratados'
+
+    # Defina os diretórios de treinamento e validação
+    train_path = diretorio_raiz+'/train_path'
+    validation_path = diretorio_raiz+'/validation_path'
+
+    TRAINING_DIR = train_path
+    training_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+    VALIDATION_DIR = validation_path
+    validation_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = training_datagen.flow_from_directory(
+        TRAINING_DIR,
+        target_size=(96, 96),
+        class_mode='categorical',
+        batch_size=126
+    )
+
+    validation_generator = validation_datagen.flow_from_directory(
+        VALIDATION_DIR,
+        target_size=(96, 96),
+        class_mode='categorical',
+        batch_size=126
+    )
+
+    model = tf.keras.models.Sequential([
+        # Note the input shape is the desired size of the image 96x96 with 3 bytes color
+        # This is the first convolution
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu',
+                            input_shape=(96, 96, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # The second convolution
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # The third convolution
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # The fourth convolution
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # Flatten the results to feed into a DNN
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        # 512 neuron hidden layer
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(17, activation='softmax')
+    ])
+
+    summary = model.summary()
+
+    score = model.compile(loss='categorical_crossentropy',
+                optimizer='rmsprop', metrics=['accuracy'])
+    
+    history = model.fit(train_generator, epochs=25, steps_per_epoch=20, 
+                        validation_data = validation_generator, verbose = 1, validation_steps=3)
+    
+    with open("C:/Users/thiago.silva/API/Projeto/Sistemas_Inteligentes/models/toxic_to_pet.pkl", "wb") as f:
+        pickle.dump(model, f)
+
+    return score, history, summary
+
+if __name__ == "__main__":
+    treino()
+
+
+
+
+    
